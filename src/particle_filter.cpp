27b3269cdb2da44bb85899f7b02598caa8b54037
Frank Carey <fcarey@gmail.com>
2017-12-24 13:07:40 -0500

    Implement the dataAssociation() function.

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -82,12 +82,60 @@ void ParticleFilter::prediction(double delta_t, double std_pos[], double velocit
 	}
 }
 
-void ParticleFilter::dataAssociation(std::vector<LandmarkObs> predicted, std::vector<LandmarkObs>& observations) {
-	// TODO: Find the predicted measurement that is closest to each observed measurement and assign the 
-	//   observed measurement to this particular landmark.
-	// NOTE: this method will NOT be called by the grading code. But you will probably find it useful to 
-	//   implement this method and use it as a helper during the updateWeights phase.
+//void ParticleFilter::dataAssociation(std::vector<LandmarkObs> predicted, std::vector<LandmarkObs>& observations) {
+//	// TODO: Find the predicted measurement that is closest to each observed measurement and assign the
+//	//   observed measurement to this particular landmark.
+//	// NOTE: this method will NOT be called by the grading code. But you will probably find it useful to
+//	//   implement this method and use it as a helper during the updateWeights phase.
+//
+//}
 
+void ParticleFilter::dataAssociation(std::vector<LandmarkObs>& predicted, std::vector<LandmarkObs>& observations, Map map_landmarks)
+{
+
+	for (int sensorCount = 0; sensorCount < observations.size(); ++sensorCount)
+	{
+		double min_dist = INFINITY;
+		// transform each sensor measurement ,which is in local(car) coordinates to global(map) coordinates
+		/*
+		Equation to transform car coordinates to map coordinates
+		|Xmap| = |cos(theta)  -sin(theta)   Xparticle |      | Xcar |
+		|Ymap| = |sin(theta)   cos(theta)   Yparticle |   X  | Ycar |
+		|  1 | = |    0             0            1    |      |   1  |
+
+		*/
+		//// use above equation for each sensor
+		//observations[sensorCount].x = particles.at(ii).x + (cos(particles.at(ii).theta) * (observations[sensorCount].x)) - (sin(particles.at(ii).theta) * (observations[sensorCount].y));
+		//observations[sensorCount].y = particles.at(ii).y + (sin(particles.at(ii).theta) * (observations[sensorCount].x)) + (cos(particles.at(ii).theta) * (observations[sensorCount].y));
+
+		for (int landmarkCount = 0; landmarkCount < map_landmarks.landmark_list.size(); ++landmarkCount)
+		{
+			// TODO## restrict far away landmarks suing sensor range
+
+			// Calculate euclidean distance between each landmark pos and sensor pos
+			//double dist_euclid = sqrt(pow(map_landmarks.landmark_list[landmarkCount].x_f - observations[sensorCount].x, 2) +
+			//					 pow(map_landmarks.landmark_list[landmarkCount].y_f - observations[sensorCount].y, 2));
+			double dist_euclid = dist(map_landmarks.landmark_list[landmarkCount].x_f, map_landmarks.landmark_list[landmarkCount].y_f,
+																observations[sensorCount].x, observations[sensorCount].y);
+
+			// if this distance is less than min distance,associate current particle with that landmark index
+			if (dist_euclid < min_dist)
+			{
+				// change the min distance to calculated euclidean distance
+				min_dist = dist_euclid;
+
+				// add the landmark index to predicted vector id
+				observations[sensorCount].id = map_landmarks.landmark_list[landmarkCount].id_i;
+			}
+
+		}
+		//cout << "transf_obs ( " << observations[sensorCount].x << " " << observations[sensorCount].y << "--->"
+		//	<< "landmark position : ( " << map_landmarks.landmark_list[observations[sensorCount].id-1].x_f << " " <<
+		//	map_landmarks.landmark_list[observations[sensorCount].id-1].y_f << " )" << " " << "ID: " <<
+		//observations[sensorCount].id << endl;
+		// now add the sensor measurement with its associated id to predicted vector
+		predicted.push_back(observations[sensorCount]);
+	}
 }
 
 void ParticleFilter::updateWeights(double sensor_range, double std_landmark[], 
